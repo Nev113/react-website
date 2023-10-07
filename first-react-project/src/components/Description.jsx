b8b684567ef9e6d13b475366a6ef5321a9fa32c8
Nev113 <nevosangkane446@gmail.com>
2023-10-07 22:50:58 +0700

    mengimpelemtasikan project sebelumnya ke sekarang.

---
+++ first-react-project/src/components/Description.jsx
@@ -0,0 +1,3 @@
+export default function Description({author}) {
+    return <p>Author: {author ? author : 'Person'}</p>
+};
---
+++ first-react-project/src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header({author}) {
+    return (<h1>React JS And {author ? author : 'Person'}</h1>)
+};
